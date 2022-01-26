195ba2184eeb3f895aaf78eb1bd97af93564ea35
Mosi8 <mauriziomosi8@gmail.com>
2022-01-26 15:41:49 +0100

    completo

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="https://picsum.photos/500/300">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+img {
+  width: 500px;
+}
+</style>
